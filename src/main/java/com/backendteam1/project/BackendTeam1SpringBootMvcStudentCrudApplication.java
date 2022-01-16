889e924fb9c17c9d5ef269e625aed007f00d4197
BackendTeam1 <97586855+BackendTeam1@users.noreply.github.com>
2022-01-16 17:27:09 +0530

    Updated ...CrudApplication.java
    Added command with arguments to scan for the classes in other packages, since api was throwing 404 error not found before this, 
    regards,
    Kushal Sharma

--- src/main/java/com/backendteam1/project/BackendTeam1SpringBootMvcStudentCrudApplication.java
+++ src/main/java/com/backendteam1/project/BackendTeam1SpringBootMvcStudentCrudApplication.java
@@ -3,7 +3,9 @@ package com.backendteam1.project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.backendteam1.project.controller",
+		"com.backendteam1.project.service.impl","com.backendteam1.project.repository",
+		"com.backendteam1.project.exception"})
 public class BackendTeam1SpringBootMvcStudentCrudApplication {
 
 	public static void main(String[] args) {
