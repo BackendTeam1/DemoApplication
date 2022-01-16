869e0c03652574526819a5253c138c7ab66b7528
BackendTeam1 <97586855+BackendTeam1@users.noreply.github.com>
2022-01-16 17:19:59 +0530

    Updated Student.java (model class)
    Added getters and setters since data was not storing in the database before hence added,
    Removed commented contact number field since it was not necessary

--- src/main/java/com/backendteam1/project/model/Student.java
+++ src/main/java/com/backendteam1/project/model/Student.java
@@ -27,7 +27,39 @@ public class Student {
 	@Column(name = "email_id", nullable = false)
 	private String email;
 	
-//	@Column(name = "contact_no")
-//	private String contactNumber;
+		
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
 
 }
